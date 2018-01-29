9e2cc694bbd0951ed4fd46c9ecef42d8b96e37f0
Chintan Mehta <mchintan24@gmail.com>
2018-01-29 16:19:53 +0530

    register button

--- app/src/main/java/com/example/android/ckho/Register.java
+++ app/src/main/java/com/example/android/ckho/Register.java
@@ -3,8 +3,10 @@ package com.example.android.ckho;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
 
@@ -24,6 +26,7 @@ public class Register extends AppCompatActivity {
         dateTime=(TextView)findViewById(R.id.date_time);
         location=(TextView)findViewById(R.id.location);
         description=(TextView)findViewById(R.id.description);
+        registerBtn=(Button)findViewById(R.id.register_button);
 
         eventName.setText(intent.getStringExtra("eventName"));
         dateTime.setText(intent.getStringExtra("date"));
@@ -31,5 +34,15 @@ public class Register extends AppCompatActivity {
         description.setText("Random Decription from server");
 
 
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                onClickRegister(v);
+            }
+        });
+    }
+    public void onClickRegister(View v)
+    {
+        Toast.makeText(this, "Registered", Toast.LENGTH_SHORT).show();
     }
 }
