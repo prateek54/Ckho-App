8234ae667b034916f76a9bc3e72cf947b8fbacca
Chintan Mehta <mchintan24@gmail.com>
2018-01-29 16:51:57 +0530

    ui fixes

--- app/src/main/java/com/example/android/ckho/Register.java
+++ app/src/main/java/com/example/android/ckho/Register.java
@@ -1,6 +1,7 @@
 package com.example.android.ckho;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,10 +9,13 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import in.shadowfax.proswipebutton.ProSwipeButton;
+
 public class Register extends AppCompatActivity {
 
     private TextView eventName,dateTime,location,description;
-    private Button registerBtn;
+    private ProSwipeButton registerBtn;
 
 
 
@@ -20,13 +24,17 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+//        int primary = getResources().getColor(R.color.colorPrimary);
+//        int secondary = getResources().getColor(R.color.colorPrimaryDark);
+//        Slidr.attach(this, primary, secondary);
+
         Intent intent = getIntent();
 
-        eventName=(TextView)findViewById(R.id.event_name);
-        dateTime=(TextView)findViewById(R.id.date_time);
-        location=(TextView)findViewById(R.id.location);
-        description=(TextView)findViewById(R.id.description);
-        registerBtn=(Button)findViewById(R.id.register_button);
+        eventName = (TextView) findViewById(R.id.event_name);
+        dateTime = (TextView) findViewById(R.id.date_time);
+        location = (TextView) findViewById(R.id.location);
+        description = (TextView) findViewById(R.id.description);
+        registerBtn = (ProSwipeButton) findViewById(R.id.register_button);
 
         eventName.setText(intent.getStringExtra("eventName"));
         dateTime.setText(intent.getStringExtra("date"));
@@ -34,15 +42,33 @@ public class Register extends AppCompatActivity {
         description.setText("Random Decription from server");
 
 
-        registerBtn.setOnClickListener(new View.OnClickListener() {
+        /**registerBtn.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v) {
-                onClickRegister(v);
+         public void onClick(View v) {
+         onClickRegister(v);
+         }
+         });**/
+
+
+        registerBtn.setOnSwipeListener(new ProSwipeButton.OnSwipeListener() {
+            @Override
+            public void onSwipeConfirm() {
+                // user has swiped the btn. Perform your async operation now
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // task success! show TICK icon in ProSwipeButton
+                        onClickRegister();
+                        registerBtn.showResultIcon(true); // false if task failed
+                    }
+                }, 1000);
             }
         });
+
     }
-    public void onClickRegister(View v)
+    public void onClickRegister()
     {
         Toast.makeText(this, "Registered", Toast.LENGTH_SHORT).show();
+
     }
 }
