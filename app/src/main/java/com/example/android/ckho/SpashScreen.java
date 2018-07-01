b8fdb208010509a317ed691cde89cc46949dd1c9
Your Name <you@example.com>
2018-07-02 00:16:16 +0530

    update2

--- app/src/main/java/com/example/android/ckho/SpashScreen.java
+++ app/src/main/java/com/example/android/ckho/SpashScreen.java
@@ -1,6 +1,7 @@
 package com.example.android.ckho;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
@@ -15,11 +16,12 @@ import android.view.WindowManager;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class SpashScreen extends AppCompatActivity {
+public class SpashScreen extends Activity {
     boolean firstStart;
     SharedPrefApp sharedPref;
 
-
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,22 +29,38 @@ public class SpashScreen extends AppCompatActivity {
 
         sharedPref = SharedPrefApp.getInstance();
 
-        for(int i=0;i<1000;i++)
+       /* for(int i=0;i<1000;i++)
         {
-        }
-
-        SharedPreferences settings = getSharedPreferences("PREFS",0);
-        firstStart = settings.getBoolean("firs_time_start", true);
-
-        if(sharedPref.getISLogged_IN(SpashScreen.this))
-        {
-            startActivity(new Intent(this,Login.class));//change to main class after saving login info
-        }
-        else
-        {
-            startActivity(new Intent(this,Login.class));
-        }
+        }*/
 
 
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                SharedPreferences settings = getSharedPreferences("PREFS",0);
+                firstStart = settings.getBoolean("firs_time_start", true);
+
+                if(sharedPref.getISLogged_IN(SpashScreen.this))
+                {
+                    startActivity(new Intent(SpashScreen.this,Login.class));//change to main class after saving login info
+                }
+                else
+                {
+                    startActivity(new Intent(SpashScreen.this,Login.class));
+                }
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
+
 }
